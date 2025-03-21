d2c15289a59a7eb25b7cdc2ce550ac8ebb24de98
alex-njugi <alex.karanja4@student.moringaschool.com>
2025-03-21 09:49:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Success:", data);
+      document.body.innerHTML = `<p>Success! User ID: ${data.id}</p>`;
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      document.body.innerHTML = `<p>Something went wrong: ${error.message}</p>`;
+    });
+  }
+  
